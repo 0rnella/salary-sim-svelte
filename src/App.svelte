bc4d35875d11b471fd8619912d970ae9a7f6ef01
friggito <ornella.friggit@gmail.com>
2022-08-05 13:36:42 +0200

    lint(*): use prettier to format

--- src/App.svelte
+++ src/App.svelte
@@ -1,47 +1,44 @@
 <script lang="ts" context="module">
-  export type Finances = {
-    monthlySpend?: number;
-    debt?: number;
-    debtRepaymentMonths?: number;
-    incomeTaxRate?: number;
-    yearlySpend?: number;
-    salary: number;
-  }
+    export type Finances = {
+        monthlySpend?: number;
+        debt?: number;
+        debtRepaymentMonths?: number;
+        incomeTaxRate?: number;
+        yearlySpend?: number;
+        salary: number;
+    };
 </script>
 
 <script lang="ts">
-  import Visualization from './lib/Visualization.svelte'
-  import SalarySelector from './lib/SalarySelector.svelte'
-  import Form from './lib/Form.svelte'
+    import Visualization from './lib/Visualization.svelte';
+    import SalarySelector from './lib/SalarySelector.svelte';
+    import Form from './lib/Form.svelte';
 
-  const processForm = ({target}) => {
-    const formData = new FormData(target);
-    for (const entry of formData.entries()) {
-      const [name, value] = entry;
-      finances[name] = Number(value);
-    }
-  }
+    const processForm = ({ target }) => {
+        const formData = new FormData(target);
+        for (const entry of formData.entries()) {
+            const [name, value] = entry;
+            finances[name] = Number(value);
+        }
+    };
 
-  const updateSalary = ({target}) => {
-    finances.salary = target.value;
-  }
+    const updateSalary = ({ target }) => {
+        finances.salary = target.value;
+    };
 
-  let finances: Finances = { monthlySpend: 0, incomeTaxRate: 25, salary: 0 };
-
+    let finances: Finances = { monthlySpend: 0, incomeTaxRate: 25, salary: 0 };
 </script>
 
 <main>
-
-  <h1>Salary Sim</h1>
-  <Form finances={finances} processForm={processForm} />
-  {#key finances}
-    {#if finances.monthlySpend}
-      <SalarySelector finances={finances} updateSalary={updateSalary} />
-      <Visualization finances={finances} />
-    {/if}
-  {/key}
+    <h1>Salary Sim</h1>
+    <Form {finances} {processForm} />
+    {#key finances}
+        {#if finances.monthlySpend}
+            <SalarySelector {finances} {updateSalary} />
+            <Visualization {finances} />
+        {/if}
+    {/key}
 </main>
 
 <style>
-
 </style>
--- src/lib/SalarySelector.svelte
+++ src/lib/SalarySelector.svelte
@@ -1,10 +1,8 @@
 <script lang="ts">
     export let finances, updateSalary;
 </script>
-  
+
 <div>
     <label for="salary">What is your desired salary?</label>
-    <input type="number" value={finances.salary} on:change={updateSalary}>
-
+    <input type="number" value={finances.salary} on:change={updateSalary} />
 </div>
-  
